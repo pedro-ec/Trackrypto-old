c1b5f21bab070075eed19fae98406bd65998e061
p.enguitacarrascon@gmail.com <p.enguitacarrascon@gmail.com>
2020-11-12 18:09:51 +0100

    Add singleton model

---
+++ Trackrypto/Model/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trackrypto.Model.Entities;
+
+namespace Trackrypto.Model
+{
+    public class Model
+    {
+        #region singleton
+        private Model()
+        {
+            Transacciones = new List<Transaccion>();
+        }
+
+        private static Model model;
+
+        public static Model GetModel()
+        {
+            if (model == null) model = new Model();
+            return model;
+        }
+        #endregion
+
+        public List<Transaccion> Transacciones { get; }
+
+
+
+    }
+}
